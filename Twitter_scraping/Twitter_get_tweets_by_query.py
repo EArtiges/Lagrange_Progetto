bbf9764301165e3328a737a78f8ce831f326b818
Emmanuel Artiges <eartiges@wombat.isi.it>
2019-02-14 18:53:25 +0100

    Converted NTF for Twitter. Re-wrote the query for spatial grid management and several words to query.

--- Twitter_scraping/Twitter_get_tweets_by_query.py
+++ Twitter_scraping/Twitter_get_tweets_by_query.py
@@ -7,6 +7,9 @@ import pandas as pd
 import twython
 from twython import TwythonRateLimitError
 import Twitter_scraping as scraper
+import string
+import pickle as pkl
+import numpy as np
 
 APP_KEY = 'ZIvxV0lZJnQI2FOuEqi0zzQqU'
 APP_SECRET = '28z7HZuCNX71NMc5TO8ga3woravEt5wFsvm7Z2Q7LERBpoSCno'
@@ -14,20 +17,83 @@ twitter = twython.Twython(APP_KEY, APP_SECRET, oauth_version=2)
 ACCESS_TOKEN = twitter.obtain_access_token()
 twitter = twython.Twython(APP_KEY, access_token=ACCESS_TOKEN)
 
-last_id=None
-search_parameters = dict(q="#sansalvario" , count = 100, max_id = last_id, bounding_box='7.582325,45.010224,7.764212,45.114893')
-statuses=scraper.query_by_word(search_parameters, twitter)
+print('Grid')
+# Define the grid we want to work with.
+Grid=pkl.load(open('../Results/2019-02-05_8_5000_diy_word2vec/pickle/Grid.pkl', 'rb'))
+latitudes=set([g[0] for g in Grid])
+longitudes=set([g[1] for g in Grid])
+Grid=[]
+for lat in sorted(latitudes):
+    for lon in sorted(longitudes):
+        Grid.append((lat,lon))
+grid_str=[str(g[0])+','+str(g[1])+',0.25km' for g in Grid]
+
+print('queries')
+# Run the queries we want on the grid we choose
+statuses=[]
+for query in ['torino','san salvario','nizza','porta nuova','piazza castello','mole','vanchiglia','vanchiglietta','lingotto','san donato','borgo crimea','bordo po','santa rita','mirafiori','crocetta','cenisia']:
+    for g in grid_str:
+        last_id=None
+        search_parameters = dict(q=query , count = 100, max_id = last_id, geocode=g)
+        print("Starting to query tweets")
+        statuses+=scraper.query_by_word(search_parameters, twitter)
+
+print('filtering')
+# For all the tweets we got, keep only the interesting info
+tweets=[]
+for status in statuses:
+    rt = 'retweeted_status' in status
+    text = status['retweeted_status']['text'] if rt else status['text']
+    status['geo'] = status['retweeted_status']['geo'] if rt else status['geo']
+    try:
+        loc=status['geo']['coordinates'] if status['geo'] else status['query_loc']
+        loc=','.join([str(i) for i in [loc]])
+    except KeyError:
+        loc=status['query_loc']
+    tweets.append((np.uint64(status['id_str']),
+                   text,
+                   rt,
+                   status['created_at'],
+                   status['lang'],loc,
+                   np.uint64(status['user']['id_str'])))
+    
+print('making/pickling dataframe')
+# Store everything in a dataset and pickle it
+df = pd.DataFrame(tweets,
+              columns=['status_id', 'text', 'rt', 'created_at', 'lang', 'location','user_id',])
+df = df.set_index('status_id')
+df.created_at = pd.to_datetime(df.created_at)
+df.to_pickle('dataframe_query.pkl')
+
+print('list of users')
+#Make a list of users and build their spatial profiles
+user_loc=[e for e in zip(df.user_id.tolist(), df.location.tolist())]
+user_loc_dict={}
+for e in user_loc:
+    if not e[0] in user_loc_dict:
+        user_loc_dict[e[0]]=[e[1]]
+    else: 
+        user_loc_dict[e[0]].append(e[1])
+for e in user_loc_dict:
+    user_loc_dict[e]=scraper.spatial_profile(user_loc_dict[e])
+    
+print('pickling dictionary')
+# Save the dicionary of users spatial profiles
+pkl.dump(user_loc_dict, open('user_loc_dict.pkl', 'wb'))
 
 
+print("Query executed. Saving files in a JSON")
 with gzip.open('torino_2019.json.gz', 'wt') as gf:
     for status in statuses:
         gf.write(json.dumps(status) + "\n")
-
-
-ID = set([s['user']['id'] for s in statuses])
+        
+print("Starting to query users.")
+# Scrape users timelines
+ID = list(user_loc_dict.keys())
 date_start = parser.parse('2000-02-01 00:00:00+00:00')
 print(len(ID), 'users to scrape')
 
 for user_ID in ID:
+    print("scraping user {}".format(user_ID))
     df=scraper.get_user_timeline(user_ID,date_start,twitter)
     df.to_pickle('USERS/dataframe_user_{}.pkl'.format(user_ID))
--- Twitter_scraping/Twitter_scraping.py
+++ Twitter_scraping/Twitter_scraping.py
@@ -7,6 +7,7 @@ import pandas as pd
 import twython
 from twython import TwythonRateLimitError
 import numpy as np
+import pickle as pkl
 
 def query_by_word(search_parameters,twitter):
     searching = True
@@ -16,7 +17,7 @@ def query_by_word(search_parameters,twitter):
         # check for rate limit
         time.sleep(.2)
         #search_parameters = dict(q="#sansalvario" , count = 100, max_id = last_id, bounding_box='7.672325,45.050224,7.684212,45.054893')
-        #print ("Searching with parameters: ", search_parameters)
+        print ("Searching with parameters: ", search_parameters)
         # search for tweets
         try:
             result = twitter.search(**search_parameters)
@@ -24,19 +25,24 @@ def query_by_word(search_parameters,twitter):
             time.sleep(901)
             continue
         # add the statuses to a list
+#        print(len(result['statuses']), "tweets collected")
         if len(result['statuses'])==0:
             break
         else:
             for status in result['statuses']:
+                status['query_loc']=search_parameters['geocode']
+                status['user']['query_loc']=search_parameters['geocode']
                 statuses.append(status)
                 last_result = status
             # update max_id
         last_id = int(last_result['id_str']) - 1
+        search_parameters['max_id']=last_id
+    print(len(statuses), "tweets collected.")
     return statuses
 
 
 def get_user_timeline(user_ID,date_start,twitter):
-    print('searching for user {}'.format(user_ID))
+    #print('searching for user {}'.format(user_ID))
     searching = True
     last_id=None
     statuses=[]
@@ -63,7 +69,7 @@ def get_user_timeline(user_ID,date_start,twitter):
 
     tweets = []
 
-    for status in statuses:      
+    for status in statuses:
         rt = 'retweeted_status' in status
         text = status['retweeted_status']['text'] if rt else status['text']
         try:
@@ -76,14 +82,23 @@ def get_user_timeline(user_ID,date_start,twitter):
                        status['created_at'],
                        status['lang'],loc,
                        np.uint64(status['user']['id_str'])))
-        
-    with gzip.open('USERS/timeline_user_{}.json.gz'.format(user_ID), 'wt') as gf:
-        for tweet in tweets:
-            gf.write(json.dumps(tweet) + "\n")
-        
     df = pd.DataFrame(tweets,
                   columns=['status_id', 'text', 'rt', 'created_at', 'lang', 'location','user_id',])
     df = df.set_index('status_id')
     df.created_at = pd.to_datetime(df.created_at)
+    with open('USERS/timeline_user_{}.pkl'.format(user_ID), 'wb') as f:
+        pkl.dump(statuses,f)
     return df
+        
+def spatial_profile(locs):
+    try:
+        return [(l,len([i for i in locs if i==l])) for l in set(locs)]
+    except TypeError:
+        loc2=[]
+        for l in locs:
+            if l['coordinates']:
+                l=l['coordinates']
+            loc2.append(l)
+        locs=loc2
+        return [(l,len([i for i in locs if i==l])) for l in set(locs)]
 
